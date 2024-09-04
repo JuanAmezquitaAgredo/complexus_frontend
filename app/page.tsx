be72c44dfcd5616c52af30f9bca7130ca7b66691
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-09-04 10:29:18 -0500

    Create Example Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import InputField from "./components/common/input/input";
 
 export default function Home() {
   return (
     <div>
       <h1>Hola mundo desde home page</h1>
+      <InputField label="Email" type="email" name="email" value="" placeholder="Email"/>
     </div>
   );
 }
