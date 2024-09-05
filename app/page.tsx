ac2f89eb2effc7f711f2973176ecff5b4768e702
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-09-05 11:44:00 -0500

    create_view_login

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import Login from "./components/Login/Login";
-
 export default function Home() {
   return (
     <div>
-      <h1>Hola mundo desde home page</h1>
-      <Login/>
+      <h1>home</h1>
     </div>
   );
 }
