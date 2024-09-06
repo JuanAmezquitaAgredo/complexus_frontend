c65035f63f349c6f43f15248051f6588c0ffb8f7
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-09-06 12:04:32 -0500

    update and changes styles and form

--- app/components/Login-component/Login.tsx
+++ app/components/Login-component/Login.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import InputField from "../common/input/input";
 import Button from "../common/button/button";
-import ButtonGradient from "../common/buttonGradient/buttonGradient";
 import Style from "./style.module.css";
 
 const Login: React.FC = () => {
@@ -11,10 +10,6 @@ const Login: React.FC = () => {
             <InputField label="Email" type="email" name="email" value="" placeholder="Email"/>
             <InputField label="Password" type="password" name="password" value="" placeholder="Password"/>
             <Button label="Login"/>
-            <div className={Style.RegisterContainer}>
-                <ButtonGradient label="Owner Register"/>
-                <ButtonGradient label="Admin Register"/>
-            </div>
         </div>
     );
 };
--- app/components/common/button/button.tsx
+++ app/components/common/button/button.tsx
@@ -2,9 +2,9 @@ import { ButtonProps } from "@/app/types/common/common";
 import React from "react";
 import  Style  from "./style.module.css";
 
-const Button: React.FC<ButtonProps> = ({ label, backgroundColor }) => {
+const Button: React.FC<ButtonProps> = ({ label, backgroundColor}) => {
     return (
-        <button className={Style.button} style={{ backgroundColor: backgroundColor }}>
+        <button className={Style.button} style={{ backgroundColor: backgroundColor  }} >
             {label}
         </button>
     );
