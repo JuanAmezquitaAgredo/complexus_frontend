024353c52dbac100f875b8cecb023ae42d283669
Juan Calderon <juanestebancredifacil@gmail.com>
2024-09-09 09:01:47 -0500

    feat: Add ProfileCard component and its styles
    This commit adds the ProfileCard component and its associated styles. The ProfileCard component is used to display user profile information, including name, email, and ID. It also includes functionality for editing the profile and logging out. The styles for the ProfileCard are defined in the Profile.module.css file.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,48 +1,62 @@
+'use client';
+
 import { Navbar } from '@/app/components/navbar/navbar'
 import PostCard from '@/app/components/postcard/PostCard'
 import { Sidebar } from '@/app/components/sidebar/sidebar'
 import React from 'react'
 import styles from './styles.module.css';
+import ProfileCard from '../components/profile/Profile';
 
 
 const adminPage = () => {
+
+  const handleEdit = () => {
+    // Lógica para editar perfil
+    alert("Edit profile");
+  };
+
+  const handleLogout = () => {
+    // Lógica para logout
+    alert("Logged out");
+  };
+
   return (
     <main>
       <Navbar />
-        <div className={styles.container}>
-            <Sidebar />
-            <div className={styles.posts}>
-              <PostCard title="Pago por trabajo"
-                user="Adrian Florez"
-                timePosted="48m"
-                description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
-                likes={1}
-                comments={4}
-                />
-                <PostCard title="Pago por trabajo"
-                user="Adrian Florez"
-                timePosted="48m"
-                description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
-                likes={1}
-                comments={4}
-                />
-                <PostCard title="Pago por trabajo"
-                user="Adrian Florez"
-                timePosted="48m"
-                description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
-                likes={1}
-                comments={4}
-                />
-                <PostCard title="Pago por trabajo"
-                user="Adrian Florez"
-                timePosted="48m"
-                description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
-                likes={1}
-                comments={4}
-                />
-                
-            </div>
+      <div className={styles.container}>
+        <Sidebar />
+        <div className={styles.posts}>
+          <PostCard title="Pago por trabajo"
+            user="Adrian Florez"
+            timePosted="48m"
+            description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
+            likes={1}
+            comments={4}
+          />
+          <PostCard title="Pago por trabajo"
+            user="Adrian Florez"
+            timePosted="48m"
+            description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
+            likes={1}
+            comments={4}
+          />
+          <PostCard title="Pago por trabajo"
+            user="Adrian Florez"
+            timePosted="48m"
+            description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
+            likes={1}
+            comments={4}
+          />
+          <PostCard title="Pago por trabajo"
+            user="Adrian Florez"
+            timePosted="48m"
+            description="¿Alguien sin empleo? Disponible para ganarse una buena liga escribir al privado solo hombres y al chat privado (trecients.k)"
+            likes={1}
+            comments={4}
+          />
+
         </div>
+      </div>
     </main>
   )
 }
---
+++ app/components/profile/Profile.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+import styles from './Profile.module.css';
+import InputField from '../common/input/input';
+
+
+interface ProfileCardProps {
+  name: string;
+  email: string;
+  id: string;
+  onEdit: () => void;
+  onLogout: () => void;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({ name, email, id, onEdit, onLogout }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <h3>Profile</h3>
+        <button className={styles.editButton} onClick={onEdit}>
+          <img className={styles.iconsB} src="/edit.png" alt="" />
+        </button>
+      </div>
+      <div className={styles.avatarContainer}>
+        <div className={styles.avatar}>
+          <img className={styles.iconsA} src="/user-card.png" alt="User avatar" />
+        </div>
+      </div>
+      <div className={styles.inputs}>
+        <InputField label="Name" type="text" name="name" value={name} placeholder="Name" boderColor="black" />
+        <InputField label="Email" type="email" name="email" value={email} placeholder="Email" boderColor="black" />
+        <InputField label="ID" type="text" name="id" value={id} placeholder="ID" boderColor="black" />
+      </div>
+      <button className={styles.logoutButton} onClick={onLogout}>
+        <img className={styles.icons} src="/logout.png" alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default ProfileCard;
