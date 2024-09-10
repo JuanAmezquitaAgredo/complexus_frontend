75dcc0da3a9f8dc82a0062ddc4d95c862f2764f3
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-09-10 10:18:25 -0500

    new merge

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,10 +1,37 @@
 'use client';
-import React from 'react'
-import style from './styles.module.css'
-import { Sidebar } from '../sidebar/sidebar';
-import PostCard from '../postcard/PostCard';
+import React, { useState } from 'react'; // Asegúrate de importar el componente ProfileCard
+import style from './styles.module.css';
+import ProfileCard from '../profile/Profile';
+import Modal from '../common/modal/modal';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import { getAuth, signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 export const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleEditProfile = () => {
+    alert('Editar perfil');
+    // Aquí puedes agregar la lógica para editar el perfil
+  };
+
+  const handleLogout = async () => {
+    const auth = getAuth(); // Obtener la instancia de auth
+    try {
+      await signOut(auth); // Cerrar sesión
+      alert('Sesión cerrada exitosamente');
+      
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+      alert('Error al cerrar sesión');
+    }
+  };
+
   return (
     <><nav className={style.nav}>
       <div>
