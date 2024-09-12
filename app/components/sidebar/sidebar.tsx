0ecf928f4cbadec15d312ca165530bd09ffa636c
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-09-12 07:32:46 -0500

    changes styles

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -8,17 +8,26 @@ import PushPinIcon from '@mui/icons-material/PushPin';
 export const Sidebar = () => {
   return (
     <aside className={style.aside}>
-        <ul className={style.ul}>
+      <ul className={style.ul}>
+        <div className={style.aside_nav}>
           <a className={style.a} href="/posts">
-            <PostAddOutlinedIcon className={style.icons}/>
+            <PostAddOutlinedIcon className={style.icons} />
           </a>
+          <h2 className={style.aside_nav_h2}>Home</h2>
+        </div>
+        <div className={style.aside_nav}>
           <a className={style.a} href="#">
-            <MapsHomeWorkOutlinedIcon className={style.icons}/>
+            <MapsHomeWorkOutlinedIcon className={style.icons} />
           </a>
+          <h2 className={style.aside_nav_h2}>Create post</h2>
+        </div>
+        <div className={style.aside_nav}>
           <a className={style.a} href="#">
-            <PushPinIcon className={style.icons}/>
+            <PushPinIcon className={style.icons} />
           </a>
-        </ul>
+          <h2 className={style.aside_nav_h2}>Owners</h2>
+        </div>
+      </ul>
     </aside>
   )
 }
